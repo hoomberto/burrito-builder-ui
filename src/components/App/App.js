6889c423ca74fe4239273b1c3780695cf59a1692
Bobby Vasquez <shootbv@gmail.com>
2021-08-10 09:19:53 -0600

    Convert App from class component to functional with useState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {getOrders} from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-class App extends Component {
-  constructor(props) {
-    super();
-  }
 
-  componentDidMount() {
-    getOrders()
-      .catch(err => console.error('Error fetching:', err));
-  }
+const App = () => {
+  const [orders, setOrders] = useState('')
 
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
+  useEffect(async () => {
+    let data = await getOrders().catch(err => console.error('Error fetching:', err))
+    console.log(data)
+    setOrders(data.orders)
+  }, [])
 
-        <Orders orders={this.state.orders}/>
-      </main>
-    );
-  }
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
+    </main>
+  )
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super();
+//   }
+//
+//   // componentDidMount() {
+//   //   getOrders()
+//   //     .catch(err => console.error('Error fetching:', err));
+//   // }
+//
+//   render() {
+//     return (
+//       <main className="App">
+//         <header>
+//           <h1>Burrito Builder</h1>
+//           <OrderForm />
+//         </header>
+//
+//         <Orders orders={this.state.orders}/>
+//       </main>
+//     );
+//   }
+// }
 
 
 export default App;

4c45277a90643a6f6afbc13bd897cedbdbf3dab3
Bobby Vasquez <shootbv@gmail.com>
2021-08-10 10:54:05 -0600

    Add test for POST functionality

--- cypress/integration/user_flow_spec.js
+++ cypress/integration/user_flow_spec.js
@@ -2,13 +2,6 @@ describe('User Flow', () => {
 
   beforeEach(() => {
     cy.intercept('http://localhost:3001/api/v1/orders', {fixture: 'orders.json'})
-    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
-      statusCode: 201,
-      body: {
-        name: 'Siri',
-        ingredients: ['test1', 'test2', 'test3']
-      }
-    })
     cy.visit('localhost:3000/')
   })
 
@@ -29,6 +22,33 @@ describe('User Flow', () => {
     cy.get('#orderSection').contains('carnitas')
     cy.get('button:nth-of-type(2)').click()
     cy.contains('Order: carnitas, steak')
+  })
+
+  it('User should be presented with an error if trying to submit form without entering name in conjunction with selecting ingredients', () => {
+    cy.get('#carnitas').click()
+    cy.get('#orderSection').contains('carnitas')
+    cy.get('button:nth-of-type(2)').click()
+    cy.contains('Order: carnitas, steak')
+    cy.get('#submitBtn').click()
+    cy.contains('Please make sure your name is entered and at least one ingredient is selected.')
+  })
+
+  it('User should be able to submit an order after entering their name and selecting at least one ingredient', () => {
+    cy.intercept('http://localhost:3001/api/v1/orders')
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 201,
+      body: {
+        name: 'Successful post',
+        ingredients: ['test1', 'test2', 'test3']
+      }
+    })
+    cy.get('input').type('Siri')
+    cy.get('#carnitas').click()
+    cy.get('button:nth-of-type(2)').click()
+    cy.get('button:nth-of-type(3)').click()
+    cy.get('#submitBtn').click().then(() => {
+      cy.contains('Nothing selected')
+    })
   })
 
 })

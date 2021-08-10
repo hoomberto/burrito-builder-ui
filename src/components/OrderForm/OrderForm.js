6705372f252722f0b1bbed0f061441f38ae89496
Bobby Vasquez <shootbv@gmail.com>
2021-08-10 09:37:44 -0600

    Finish converting OrderForm to functional component

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -7,14 +7,55 @@ const OrderForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
     this.clearInputs();
+    // POST here probably, followed by a get
   }
 
   const clearInputs = () => {
     this.setState({name: '', ingredients: []});
   }
 
+  const handleIngredientChange = () => {
+    return
+  }
+
+  const renderIngredientButtons = () => {
+    const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
+    return possibleIngredients.map(ingredient => {
+      return (
+        <button key={ingredient} name={ingredient} onClick={e => handleIngredientChange(e)}>
+          {ingredient}
+        </button>
+      )
+    });
+  }
+
+  const handleNameChange = e => {
+    setName(e.target.value)
+  }
+  // 
+  // const handleSubmit = () => {
+  //   setIngredients()
+  // }
+
+
   return (
-    <></>
+    <form>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        value={name}
+        onChange={e => handleNameChange(e)}
+      />
+
+    { renderIngredientButtons() }
+
+      <p>Order: { ingredients.join(', ') || 'Nothing selected' }</p>
+
+      <button onClick={e => handleSubmit(e)}>
+        Submit Order
+      </button>
+    </form>
   )
 }
 
@@ -40,34 +81,34 @@ const OrderForm = () => {
 //   }
 //
 //   render() {
-//     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
-//     const ingredientButtons = possibleIngredients.map(ingredient => {
-//       return (
-//         <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
-//           {ingredient}
-//         </button>
-//       )
-//     });
+    // const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
+    // const ingredientButtons = possibleIngredients.map(ingredient => {
+    //   return (
+    //     <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+    //       {ingredient}
+    //     </button>
+    //   )
+    // });
 //
-//     return (
-//       <form>
-//         <input
-//           type='text'
-//           placeholder='Name'
-//           name='name'
-//           value={this.state.name}
-//           onChange={e => this.handleNameChange(e)}
-//         />
-//
-//         { ingredientButtons }
-//
-//         <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
-//
-//         <button onClick={e => this.handleSubmit(e)}>
-//           Submit Order
-//         </button>
-//       </form>
-//     )
+    // return (
+    //   <form>
+    //     <input
+    //       type='text'
+    //       placeholder='Name'
+    //       name='name'
+    //       value={this.state.name}
+    //       onChange={e => this.handleNameChange(e)}
+    //     />
+    //
+    //     { ingredientButtons }
+    //
+    //     <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+    //
+    //     <button onClick={e => this.handleSubmit(e)}>
+    //       Submit Order
+    //     </button>
+    //   </form>
+    // )
 //   }
 // }
 
